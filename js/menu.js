aa2d072fb1495e79a18cb8ee9f06e262891d7238
sazabache <sandramar_02@hotmail.com>
2016-03-14 00:08:32 -0500

    triangle-theme

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+$('li.dropdown').find('.fa-angle-down').each(function(){
+		$(this).on('click', function(){
+			if( $(window).width() < 768 ) {
+				$(this).parent().next().slideToggle();
+			}
+			return false;
+		});
+	});
